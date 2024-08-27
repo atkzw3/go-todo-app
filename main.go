39c089328035eb0d13b566d33bac3bfb62da7efa
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-27 23:22:07 +0900

    :sparkles: create todo func

--- main.go
+++ main.go
@@ -11,16 +11,13 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@test.com"
-		u.Password = "123456"
-		fmt.Println(u)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@test.com"
+	u.Password = "123456"
+	fmt.Println(u)
 
-		u.CreateUser()
-
-	*/
+	u.CreateUser()
 
 	//u, _ := models.GetUser(1)
 	//fmt.Println(u)
@@ -37,4 +34,14 @@ func main() {
 	//u2.DeleteUser()
 	//u3, _ := models.GetUser(1)
 	//fmt.Println(u3) // データがないので初期値が返ってくる
+
+	u2, _ := models.GetUser(1)
+	fmt.Println(u2)
+	c := "コンテンツ！!"
+
+	err := u2.CreateTodo(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
