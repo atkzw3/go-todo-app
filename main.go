0991808f95955adfe36d627675fe658f3ce1ee8e
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-28 22:45:28 +0900

    :sparkles: create get todo all method

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ func main() {
 	//fmt.Println(config.Config)
 	//log.Print("test")
 
-	fmt.Println(models.Db)
-
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@test.com"
-	u.Password = "123456"
-	fmt.Println(u)
-
-	u.CreateUser()
+	//fmt.Println(models.Db)
+	//
+	//u := &models.User{}
+	//u.Name = "test"
+	//u.Email = "test@test.com"
+	//u.Password = "123456"
+	//fmt.Println(u)
+	//
+	//u.CreateUser()
 
 	//u, _ := models.GetUser(1)
 	//fmt.Println(u)
@@ -46,4 +46,7 @@ func main() {
 
 	t, _ := models.GetTodo(6)
 	fmt.Println("todo", t)
+
+	todoAll, _ := models.GetTodoAll()
+	fmt.Println(todoAll)
 }
