56d2b4ed70a8adea0bfda51a829dd1e5e071a5f5
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-30 23:12:56 +0900

    :sparkles: set scc and js

--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ type List struct {
 	SQLDriver string
 	DbName    string
 	LogFile   string
+	Static    string
 }
 
 var Config List
@@ -32,5 +33,6 @@ func LoadConfig() {
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 		DbName:    cfg.Section("db").Key("name").String(),
 		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
 	}
 }
