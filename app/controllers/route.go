0cd7462fb287fe7cd376b0a0690fe263daf77aa5
atkzw3 <akiyoshi.t3@gmail.com>
2024-08-30 23:22:53 +0900

    :sparkles: set template engine

--- app/controllers/route.go
+++ app/controllers/route.go
@@ -1,19 +1,9 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, req *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-
-	data := make(map[string]interface{})
-	data["title"] = "テストです！！"
-
-	err := t.Execute(w, data)
-	if err != nil {
-		log.Println(err)
-	}
+	generateHTML(w, "helllllllo", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,26 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"todo-app/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, filename := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", filename))
+	}
+
+	// エラーの場合はpanicになるので注意
+	templates := template.Must(template.ParseFiles(files...))
+
+	err := templates.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
